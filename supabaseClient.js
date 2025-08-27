3cd1bdac2906968fbfad1baf9f7c8e3a0c285751
tgC3ren1 <mikalxbox12@gmail.com>
2025-08-27 14:51:22 +0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // Fill these with your project values (Project Settings -> API)
-window.SUPABASE_URL = window.SUPABASE_URL || "https://YOUR-PROJECT.supabase.co";
-window.SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || "YOUR-ANON-KEY";
+window.SUPABASE_URL = "https://yscynbqgjfjdcdhplszw.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzY3luYnFnamZqZGNkaHBsc3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyOTM4ODIsImV4cCI6MjA3MTg2OTg4Mn0.l9ypK28D5kBJIXInQOd6zCYdjiKIH-fvcVNdLYe_UzM";
 
 const supa = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
 
